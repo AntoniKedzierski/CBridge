972c8c3eb1e1419da10fe22a2cdcea2e2e2bd387
HubertBorzymek <leemi.smash@gmail.com>
2026-02-09 14:50:03 +0100

    force na jedno kółko check box

--- BiddingBrowser/BiddingTree/Bids/Bid.cs
+++ BiddingBrowser/BiddingTree/Bids/Bid.cs
@@ -78,6 +78,8 @@ public class Bid : BindableBase, IBidsContainer {
 
     public bool AutomaticResponse { get; set => SetProperty(ref field, value); }
 
+    public bool OneRoundForcing { get; set => SetProperty(ref field, value); }
+
     public ObservableCollection<Bid> NextBids { get; set => SetProperty(ref field, value); } = new();
 
 
